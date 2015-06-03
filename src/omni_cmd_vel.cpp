4f7619f04dddc16211de71296125a6e1346df16a
Nick Charabaruk <nicholas.charabaruk@uoit.ca>
2015-06-03 08:59:49 -0400

    Updated for new encoders and for gear ratio

--- src/omni_cmd_vel.cpp
+++ src/omni_cmd_vel.cpp
@@ -9,17 +9,18 @@ ros::Publisher rear_cmd_vel_pub;
 
 double wheel_radius = 0.125;
 
-// Max speed = 1.3 km/hr
-// Limiting speed to 1 km/hr
-// That is 21.25 rpm, roughly 96 input
+// Max speed = 1.77 m/s = 6.37 km/hr
+// Limiting speed to 1.5 m/s
+// That is 114.59 rpm, roughly 107 input
 
-static double REL_MAX = 35.0;
+static double REL_MAX = 107.0;
+static int GEAR_RATIO = 20;
 
 //function to calculate the wheel velocities
 void cmd_velCallback(const geometry_msgs::Twist::ConstPtr& msg) 
 {
   //the sum of the distance between the wheel's x-coordinate and the x-axis, and the y-coordinate and the y-axis
-  const double k = 0.47 + 0.55;  
+  const double k = 0.47 + 0.55;
 	
   // Copy the velocities (m/s)
   double Vx = msg->linear.x;
@@ -33,17 +34,17 @@ void cmd_velCallback(const geometry_msgs::Twist::ConstPtr& msg)
   double W4 = Vx - Vy + Wv * k; 
 	
   // Convert linear velocities (m/s) to rotational velocities (rpm)
-  double front_B_rpm = W1 * (60.0 / (2 * M_PI * wheel_radius));
-  double front_A_rpm = W2 * (60.0 / (2 * M_PI * wheel_radius));
-  double rear_B_rpm = W3 * (60.0 / (2 * M_PI * wheel_radius));
-  double rear_A_rpm = W4 * (60.0 / (2 * M_PI * wheel_radius));
+  double front_B_rpm = W1 * (60.0 / (2 * M_PI * wheel_radius)) * GEAR_RATIO;
+  double front_A_rpm = W2 * (60.0 / (2 * M_PI * wheel_radius)) * GEAR_RATIO;
+  double rear_B_rpm = W3 * (60.0 / (2 * M_PI * wheel_radius)) * GEAR_RATIO;
+  double rear_A_rpm = W4 * (60.0 / (2 * M_PI * wheel_radius)) * GEAR_RATIO;
     
   // Convert from rpm to relative 
   // rel = (rpm * PPR * Time Base+1)/58593.75(from pg 81 of the Operating Manual)
-  double front_A_rel = front_A_rpm * 1250 * 11 / 58593.75;
-  double front_B_rel = front_B_rpm * 1250 * 11 / 58593.75;
-  double rear_A_rel = rear_A_rpm * 1250 * 11 / 58593.75;
-  double rear_B_rel = rear_B_rpm * 1250 * 11 / 58593.75;
+  double front_A_rel = front_A_rpm * 250 * 11 / 58593.75;
+  double front_B_rel = front_B_rpm * 250 * 11 / 58593.75;
+  double rear_A_rel = rear_A_rpm * 250 * 11 / 58593.75;
+  double rear_B_rel = rear_B_rpm * 250 * 11 / 58593.75;
 
   // Bounds check
   if(front_A_rel > REL_MAX || front_A_rel < -REL_MAX)
